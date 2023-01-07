60c973dc47b6bdc0ecc93c3df6665d68cf1bdd35
Tsotne91 <tsotne91@gmail.com>
2023-01-07 21:19:58 +0400

    YearlyReports to display properly in about us both mobile and desktop.
    Should add background image (img/Group 237.svg) to about us page both mobile and desktop.
    
    CareerFormModal and FormSubmit can see name of file to upload - should remove initial path name and leave only file name.
    
    Need to handle error about controlled input, which pops up when typing in the forms

--- src/components/insuranceIcons/YearlyReports.js
+++ src/components/insuranceIcons/YearlyReports.js
@@ -1,19 +1,20 @@
 import {Container, Form} from "react-bootstrap";
 import {useRef, useState} from "react";
 
-export default function YearlyReports({className}){
+export default function YearlyReports({className}) {
+
 
     const [selectedYear, setSelectedYear] = useState("2022");
     const selectElement = useRef();
 
-    const selectYear = () => {
+    const selectYear = (event) => setSelectedYear(event.target.value);
 
-    }
 
     return (
         <>
             {/*MobileVersion:*/}
-            <div className={`${className} d-flex d-md-none bg-light flex-wrap justify-content-center rounded-3 m-3 py-4`}>
+            <div
+                className={`${className} d-flex d-md-none bg-light flex-wrap justify-content-center rounded-3 m-3 py-4`}>
                 <h3 className="text-primary">წლიური ანგარიშები</h3>
                 <Form.Select name="yearly-report" className="w-50 my-2">
                     <option value="2021">2021</option>
@@ -37,19 +38,39 @@ export default function YearlyReports({className}){
                     <img src="./img/insuranceIcons/file_download_black_24dp.svg" alt="icon" className="downIcon px-2"/>
                 </div>
             </div>
-            {/*DesktopVersion:*/}
-            <Container className={`${className} d-none d-md-flex flex-column bg-light rounded-4 border border-dark my-5 py-4 w-50`}>
+            {/*DesktopVersion: should disable borders and think how to apply wallpaper to the AboutUs page*/}
+            <Container className={`${className} d-none d-md-flex flex-column bg-light rounded-4 border border-dark my-5 py-4`}>
                 <h3 className="text-primary align-self-center">წლიური ანგარიშები</h3>
                 <Form.Select aria-label="yearly-reports"
-                             className="w-50 align-self-center my-3"
+                             className="w-25 align-self-center my-3"
                              onChange={selectYear}
                              ref={selectElement}
+                             value={selectedYear}
                 >
-                    <option value="2022">{selectedYear.value}</option>
-                    <option value="2021">{selectedYear.value}</option>
-                    <option value="2020">{selectedYear.value}</option>
+                    <option value="2022">2022</option>
+                    <option value="2021">2021</option>
+                    <option value="2020">2020</option>
                 </Form.Select>
-
+                <div className="downloadReports d-flex justify-content-center">
+                    <div className="border border-dark rounded-2 text-center px-5 py-2 mx-2">
+                        <img src="./img/insuranceIcons/Group 240.svg" alt="icon"/>
+                        <h6>I კვარტალი</h6>
+                        <p className="text-secondary">{selectedYear} წელი</p>
+                        <button><img src="./img/insuranceIcons/file_download_black_24dp.svg" alt="download"/>გადმოწერა</button>
+                    </div>
+                    <div className="border border-dark rounded-2 text-center px-5 py-2 mx-2">
+                        <img src="./img/insuranceIcons/Group 240.svg" alt="icon"/>
+                        <h6>II კვარტალი</h6>
+                        <p className="text-secondary">{selectedYear} წელი</p>
+                        <button><img src="./img/insuranceIcons/file_download_black_24dp.svg" alt="download"/>გადმოწერა</button>
+                    </div>
+                    <div className="border border-dark rounded-2 text-center px-5 py-2 mx-2">
+                        <img src="./img/insuranceIcons/Group 240.svg" alt="icon"/>
+                        <h6>III კვარტალი</h6>
+                        <p className="text-secondary">{selectedYear} წელი</p>
+                        <button><img src="./img/insuranceIcons/file_download_black_24dp.svg" alt="download"/>გადმოწერა</button>
+                    </div>
+                </div>
             </Container>
 
         </>
